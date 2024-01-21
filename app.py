29e860f1809bb71234da6e0f65a6f92b5b1959d2
Gouuse21 <156297163+Gouuse21@users.noreply.github.com>
2024-01-22 04:09:02 +0600

    Update app.py
    Запись номера в бд

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
+import time
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 @app.route('/')
 def index():
@@ -11,7 +13,9 @@ def save_number():
     phone_number = request.form['phone']
     with open('Numbers.txt', 'a') as file:
         file.write(phone_number + '\n')
-    return 'Номер сохранен в базу, спасибо!'
+    flash('Номер сохранен в базу, спасибо!')
+    time.sleep(1)  # Подождать 1 секунду перед перенаправлением
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
